731acb8a441f2a55365b18efb93ce16d5ab0ceef
Sayyed Mudassir <sayyedmudassir443@gmail.com>
2025-11-09 16:18:32 +0530

    Day 1 stuffs

--- Day1.py
+++ Day1.py
@@ -1,2 +1,21 @@
-print ("Hello")
-print ("Welcome to Day 1 of Coding in Python!")
+#Topic 1: Introduction to Python - (Printing Strings)
+print("Hello, World!")
+print("This is Day 1 of the coding challenge.")
+
+#Topic 2: Variables - (To store data)
+name = "Mudassir"
+age = 20
+height = 5.9
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+#Topic 3: Strings + Concatentation - (Combining strings)
+first = "Hello"
+second = "friend"
+print(first + " " + second)   # joins strings
+
+#Topic 4: Input Function - (Getting user input)
+name = input("What's your name? ")
+print("Nice to meet you, " + name + "!")
